11067587137313be7eaea7da79932a5f6dcd3332
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-04-02 18:30:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+	const bold = document.querySelector("strong");
+	bold.forEach(word =>{
+		word.style.color = rgb(0, 128, 0);
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	const bold = document.querySelector("strong");
+	bold.forEach(word=>{
+		word.style.color = rgb(0, 0, 0);
+	})
     
 }
