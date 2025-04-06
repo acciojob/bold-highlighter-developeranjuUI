8ed982f8224f941dbd4c90e8448cb7b57bf0823f
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-04-06 23:08:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function highlight() {
     //Write your code here
-	const bold = document.querySelector("strong");
+	const bold = document.querySelectorAll("strong");
 	bold.forEach(word =>{
-		word.style.color = rgb(0, 128, 0);
-	})
+		word.style.color = 'rgb(0, 128, 0)';
+	});
 
 }
 
 
 function return_normal() {
     //Write your code here
-	const bold = document.querySelector("strong");
+	const bold = document.querySelectorAll("strong");
 	bold.forEach(word=>{
-		word.style.color = rgb(0, 0, 0);
+		word.style.color = 'rgb(0, 0, 0)';
 	})
     
 }
